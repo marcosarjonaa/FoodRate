3973f8a186178f856cc9ed13ef6b44566d64bde6
marcosarjonaa <marcosarjonacomino@gmail.com>
2024-12-13 19:37:54 +0100

    Arreglo 1.3

--- app/src/main/java/com/example/foodrate/LoginActivity.kt
+++ app/src/main/java/com/example/foodrate/LoginActivity.kt
@@ -1,6 +1,5 @@
 package com.example.foodrate
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -10,22 +9,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthEmailException
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import com.google.firebase.auth.FirebaseAuthInvalidUserException
-import com.google.firebase.auth.FirebaseAuthUserCollisionException
-import com.google.firebase.auth.FirebaseAuthWeakPasswordException
-import com.google.firebase.auth.auth
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var btnLogin : Button
-    private lateinit var btnRegister : Button
-    private lateinit var btnRecoverPass : Button
-    private lateinit var editUser : EditText
-    private lateinit var editPassword : EditText
-    private lateinit var auth : FirebaseAuth
+    private lateinit var btnLogin: Button
+    private lateinit var btnRegister: Button
+    private lateinit var btnRecoverPass: Button
+    private lateinit var editUser: EditText
+    private lateinit var editPassword: EditText
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,34 +30,34 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
+        auth = FirebaseAuth.getInstance()
+        if (estaLogueado()) {
+            navigateToMainActivity()
+            finish()
+        }
         init()
         start()
     }
 
-
-    private fun init(){
+    private fun init() {
         btnLogin = findViewById(R.id.btn_login_in_login)
         btnRegister = findViewById(R.id.btn_register_in_login)
         btnRecoverPass = findViewById(R.id.btn_recover_pass)
         editUser = findViewById(R.id.edit_user_login)
         editPassword = findViewById(R.id.edit_pass_login)
-        auth = Firebase.auth
     }
 
-
     private fun start() {
-        btnLogin.setOnClickListener{
+        btnLogin.setOnClickListener {
             val user = editUser.text.toString()
             val pass = editPassword.text.toString()
 
             if (user.isNotEmpty() && pass.isNotEmpty())
-                startLogin(user, pass){
-                        result, msg ->
+                startLogin(user, pass) { result, msg ->
                     Toast.makeText(this@LoginActivity, msg, Toast.LENGTH_LONG).show()
-                    if (result){
-                        intent = Intent(this@LoginActivity, MainActivity::class.java)
-                        startActivity(intent)
+                    if (result) {
+                        salvarLogueo(user) // Save login state
+                        navigateToMainActivity()
                         finish()
                     }
                 }
@@ -71,87 +65,84 @@ class LoginActivity : AppCompatActivity() {
                 Toast.makeText(this, "Tienes algún campo vacío", Toast.LENGTH_LONG).show()
         }
 
-        btnRegister.setOnClickListener{
-            intent = Intent (this, RegistrarActivity::class.java)
+        btnRegister.setOnClickListener {
+            val intent = Intent(this, RegistrarActivity::class.java)
             startActivity(intent)
         }
 
-        btnRecoverPass.setOnClickListener{
+        btnRecoverPass.setOnClickListener {
             val user = editUser.text.toString()
             if (user.isNotEmpty())
-                recoverPassword(user){
-                        result, msg ->
+                recoverPassword(user) { result, msg ->
                     Toast.makeText(this@LoginActivity, msg, Toast.LENGTH_LONG).show()
-                    if (!result)
-                        editUser.setText("")
+                    if (!result) editUser.setText("")
                 }
             else
                 Toast.makeText(this, "Debes rellenar el campo email", Toast.LENGTH_LONG).show()
         }
     }
 
-    /*
-    Cuando queramos la recuperación de password, al mandar un email
-    válido, firebase lo saca como satisfactorio exista o no el usuario
-    en firebase. Eso lo hace, porque no quiere que compruebe, si un usuario
-    existe en su bbdd. Es lógico.
-     */
-    private fun recoverPassword(email : String, onResult: (Boolean, String)->Unit) {
+    private fun recoverPassword(email: String, onResult: (Boolean, String) -> Unit) {
         auth.sendPasswordResetEmail(email)
-            .addOnCompleteListener{
-                    taskResetEmail ->
-                if (taskResetEmail.isSuccessful){
-                    onResult (true, "Acabamos de enviarte un email con la nueva password")
-                }else{
-                    var msg = ""
-                    try{
-                        throw taskResetEmail.exception?:Exception("Error de reseteo inesperado")
-                    }catch (e : FirebaseAuthInvalidCredentialsException){
-                        msg = "El formato del email es incorrecto"
-                    }catch (e: Exception){
-                        msg = e.message.toString()
+            .addOnCompleteListener { taskResetEmail ->
+                if (taskResetEmail.isSuccessful) {
+                    onResult(true, "Acabamos de enviarte un email con la nueva password")
+                } else {
+                    val msg = try {
+                        throw taskResetEmail.exception ?: Exception("Error de reseteo inesperado")
+                    } catch (e: FirebaseAuthInvalidCredentialsException) {
+                        "El formato del email es incorrecto"
+                    } catch (e: Exception) {
+                        e.message.toString()
                     }
                     onResult(false, msg)
-
-
                 }
             }
-
-
     }
 
     private fun startLogin(user: String, pass: String, onResult: (Boolean, String) -> Unit) {
         auth.signInWithEmailAndPassword(user, pass)
-            .addOnCompleteListener {
-                    taskAssin ->
-                var msg = ""
-                if (taskAssin.isSuccessful){
-                    //debemos comprobar si el usuario ha verificado el email
+            .addOnCompleteListener { taskAssin ->
+                if (taskAssin.isSuccessful) {
                     val posibleUser = auth.currentUser
-                    if (posibleUser?.isEmailVerified == true){
-                        onResult ( true, "Usuario Logueado satisfactoriamente")
-                    }else{
-                        auth.signOut() //hay que desloguearse, porque no ha verificado.
-                        onResult (false, "Debes verificar tu correo antes de loguearte")
+                    if (posibleUser?.isEmailVerified == true) {
+                        onResult(true, "Usuario Logueado satisfactoriamente")
+                    } else {
+                        auth.signOut()
+                        onResult(false, "Debes verificar tu correo antes de loguearte")
                     }
-                }else{
-                    try {
-                        throw taskAssin.exception?: Exception("Error desconocido")
-                    }catch (e: FirebaseAuthInvalidUserException){
-                        msg = "El usuario tiene problemas por haberse borrado o desabilitado"
-                    }catch (e: FirebaseAuthInvalidCredentialsException){
-                        msg = if (e.message?.contains("There is no user record corresponding to this identifier") == true){
+                } else {
+                    val msg = try {
+                        throw taskAssin.exception ?: Exception("Error desconocido")
+                    } catch (e: FirebaseAuthInvalidUserException) {
+                        "El usuario tiene problemas por haberse borrado o desabilitado"
+                    } catch (e: FirebaseAuthInvalidCredentialsException) {
+                        if (e.message?.contains("There is no user record corresponding to this identifier") == true) {
                             "El usuario no existe"
-                        }else "contraseña incorrecta"
-
-                    }catch (e: Exception){
-                        msg = e.message.toString()
+                        } else "Contraseña incorrecta"
+                    } catch (e: Exception) {
+                        e.message.toString()
                     }
-
-                    onResult (false, msg)  //genérico.
+                    onResult(false, msg)
                 }
-
             }
+    }
 
+    private fun salvarLogueo(email: String) {
+        val sharedPreferences = getSharedPreferences("MyAppPreferences", MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString("user_email", email)
+        editor.putBoolean("is_logged_in", true)
+        editor.apply()
+    }
+
+    private fun estaLogueado(): Boolean {
+        val sharedPreferences = getSharedPreferences("MyAppPreferences", MODE_PRIVATE)
+        return sharedPreferences.getBoolean("is_logged_in", false)
+    }
+
+    private fun navigateToMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/foodrate/MainActivity.kt
+++ app/src/main/java/com/example/foodrate/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.foodrate
 
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,17 +13,40 @@ import com.example.foodrate.controller.Controller
 import com.example.foodrate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    //private lateinit var ficheroCompartido : SharedPreferences
+    private lateinit var ficheroCompartido : SharedPreferences
     lateinit var controller: Controller
     lateinit var bindingMain : ActivityMainBinding
+    private lateinit var btnLogout: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bindingMain = ActivityMainBinding.inflate(layoutInflater);
         setContentView(bindingMain.root);
-        init()
-        
+        iniciarPreferenciasCompartidas();
+        init();
+
+        btnLogout = findViewById(R.id.btn_logout)
+
+        btnLogout.setOnClickListener {
+            logout()
+        }
+
+    }
+    private fun logout() {
+        val sharedPreferences = getSharedPreferences("MyAppPreferences", MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.clear()
+        editor.apply()
+
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun iniciarPreferenciasCompartidas(){
+        val nombreFicheroCompartido = getString(R.string.ficheroCompartido)
+        this.ficheroCompartido = getSharedPreferences(nombreFicheroCompartido, MODE_PRIVATE)
     }
 
     private fun init(){
